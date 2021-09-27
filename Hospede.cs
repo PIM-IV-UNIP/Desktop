ce1a38c2839fe78d825e899884b5abde15f9abf8
Victor Muramatsu <victor.muramatsu@gmail.com>
2021-09-27 14:37:01 -0300

    Adicionar arquivos de projeto.

---
+++ Hospede.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Desktop
+{
+    public class Hospede
+    {
+        private int idHospede { get; set; }
+
+        public Hospede()
+        {
+
+        }
+
+        public void cadastrarHospede()
+        {
+
+        }
+
+        public void consultarHospede()
+        {
+
+        }
+
+        public void atualizarHospede()
+        {
+
+        }
+    }
+}
